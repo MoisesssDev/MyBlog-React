10a1404787c7a6c26e450fe6314e9e543d94a7a3
Moises Almeida <moisesssprg@gmail.com>
2024-07-02 16:50:28 -0300

    Finaliza a navBar e adiciona mensagem de bem-vindo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,30 @@ import styles from './Header.module.css';
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <h1>MyBlog</h1>
-        <input type="text" placeholder='Search'/>
-      </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <h1>MyBlog</h1>
+          <input type="text" placeholder='Search'/>
+        </div>
 
-      <nav className={styles.navBar}>
-        <a href="#">
-          <span className='material-symbols-outlined'>
-            notifications
-          </span>
-        </a>
-        <a href="#">
-          <span className='material-symbols-outlined'>
-            account_circle
-          </span>
-        </a>
-      </nav>
-    </header>
+        <nav className={styles.navBar}>
+          <a href="#">
+            <span className='material-symbols-outlined'>
+              notifications
+            </span>
+          </a>
+          <a href="#">
+            <span className='material-symbols-outlined'>
+              account_circle
+            </span>
+          </a>
+        </nav>
+      </header>
+      <div className={styles.msgBemVindo}>
+        <p>Bem-vindo(a) ao melhor Blog de tecnologia</p>
+      </div>
+    </>
   );
 }
 
