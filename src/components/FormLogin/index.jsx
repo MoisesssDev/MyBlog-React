b2dc601244cf41d030ed4fe8a8668c341bd84b62
Moises Almeida <moisesssprg@gmail.com>
2024-07-16 13:35:53 -0300

    adiciona react hook form

--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -2,20 +2,40 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import styles from './Login.module.css';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <main>
-      <Form className={styles.form}>
+      <Form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="mb-5">Login</h2>
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="E-mail" />
+          <Form.Control 
+            type="email" 
+            placeholder="E-mail"
+            {...register("email", { required: 'e-mail obrigatório' })}
+          />
+          { errors.email && <p className="text-danger">{errors.email.message}</p> }
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" placeholder="Senha" />
+          <Form.Control 
+            type="password" 
+            placeholder="Senha"
+            {...register("password", { required: 'senha obrigatória' })}
+          />
+          { errors.password && <p className="text-danger">{errors.password.message}</p> }
         </Form.Group>
+
         <div className={styles.actions}>
           <Button variant="dark" type="submit">
             Entrar
