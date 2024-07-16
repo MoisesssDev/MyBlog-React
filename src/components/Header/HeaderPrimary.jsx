41d760703adfe882787b4b8f338d980aebd1f695
Moises Almeida <moisesssprg@gmail.com>
2024-07-16 14:01:44 -0300

    adiciona botao de voltar para a home

--- src/components/Header/HeaderPrimary.jsx
+++ src/components/Header/HeaderPrimary.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from './HeaderPrimary.module.css';
 
 function HeaderPrimary() {
   return (
     <header className="header-primary">
+      <Link className={styles.linkToHome} to="/">
+        <span className="material-symbols-outlined">arrow_back_ios</span>voltar para home
+      </Link>
       <div className={styles.logoContainer}>
           <h1>MyBlog</h1>
         </div>
